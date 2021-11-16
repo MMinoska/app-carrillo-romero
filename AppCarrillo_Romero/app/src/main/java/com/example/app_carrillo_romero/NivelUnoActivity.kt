2c1d11795fa56a527a2595aa0f15bd5411f1df7f
MMinoska <78432716+MMinoska@users.noreply.github.com>
2021-11-15 19:42:35 -0500

    a
    PRIMER COMENTARIO

---
+++ AppCarrillo_Romero/app/src/main/java/com/example/app_carrillo_romero/NivelUnoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app_carrillo_romero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NivelUnoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nivel_uno)
+    }
+}
---
+++ AppCarrillo_Romero/app/src/main/java/com/example/app_carrillo_romero/NivelesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app_carrillo_romero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NivelesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_niveles)
+    }
+}
